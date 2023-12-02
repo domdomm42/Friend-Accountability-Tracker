19c040bdbb61bb7233635ea6bbd83ff409bb7916
NathanTNguyen <nathannguyen1590@gmail.com>
2023-12-02 16:57:38 +1100

    created basic signin page and route
    created basic signin page and route.
    no functionality for signin or validation yet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SignupPage } from './pages/authentication/SignupPage';
+import { SigninPage } from './pages/authentication/SigninPage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/signup' element={<SignupPage />} />
+          <Route path='/signin' element={<SigninPage />} />
           <Route path='*' element={<div>Route not found</div>} />
         </Routes>
       </Router>
---
+++ frontend/src/pages/authentication/SigninPage.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+const CustomLink = React.forwardRef((props, ref) => (
+  <RouterLink ref={ref} {...props} />
+));
+
+CustomLink.displayName = 'CustomLink';
+
+export function SigninPage() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: Add form submission logic here
+    console.log('Form submitted');
+  };
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id='email'
+                label='Email Address'
+                name='email'
+                autoComplete='email'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+                autoComplete='password'
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container justifyContent='flex-end'>
+            <Grid item>
+              <Link href='#' variant='body2'>
+                Don't have an account? Sign up
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+    // </ThemeProvider>
+  );
+}
