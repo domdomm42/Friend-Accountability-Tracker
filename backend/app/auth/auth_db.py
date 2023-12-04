5b051e3a154379c2eb0017dc34c1a751c7b60cb1
NathanTNguyen <nathannguyen1590@gmail.com>
2023-12-04 20:35:26 +1100

    fixed signup logic which previously allowed duplicate emails and emails during signup

--- backend/app/auth/auth_db.py
+++ backend/app/auth/auth_db.py
@@ -8,12 +8,12 @@ async def register_user(user_info: User):
     await user_info.insert()
 
 async def username_is_unique(username):
-    user = await User.find_one(User.username != username)
-    return user is not None
+    user = await User.find_one(User.username == username)
+    return user is None
 
 async def email_is_unique(email):
-    user = await User.find_one(User.email != email)
-    return user is not None
+    user = await User.find_one(User.email == email)
+    return user is None
 
 async def get_user_by_email(email):
     return await User.find_one(User.email == email)
