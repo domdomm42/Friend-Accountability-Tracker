98a23ebd331b5e0f4bd6cc052b19ab75fd44a7c3
NathanTNguyen <nathannguyen1590@gmail.com>
2023-12-16 15:59:21 +1100

    updated profilepage layout and styling + updated requirements.txt

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 const ProfilePage = () => {
     const [user, setUser] = useState({});
@@ -22,12 +32,32 @@ const ProfilePage = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Welcome, {user.username}!</h1>
-            <p>Email: {user.email}</p>
-            <p>Muahahaha finally it works</p>
-            {/* Render other profile information */}
-        </div>
+        <>
+            <CssBaseline />
+            <Box sx={{ flexGrow: 1}}>
+                <AppBar position="absolute">
+                    <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>Profile</Typography>
+                    <Button color="inherit">Logout</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <Container maxWidth="xl" sx={{ padding: '60px' }}> 
+                <Typography variant="h3" sx={{ paddingBottom: '40px' }}>Welcome, {user.username}!</Typography>
+                <Typography variant="body1">Email: {user.email}</Typography>
+                <Typography variant="body1">Muahahaha finally it works</Typography>
+                {/* other profile stuff here i guess */}
+            </Container>
+        </>
     );
 };
 
